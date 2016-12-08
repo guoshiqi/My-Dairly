5034f9c9e6a42466fe3f9f9edc4b94239b5385b9
hey <466256173@qq.com>
2016-12-09 00:08:16 +0800

    greendao

--- app/src/main/java/com/name/cn/mydairly/MainActivity.java
+++ app/src/main/java/com/name/cn/mydairly/MainActivity.java
@@ -3,6 +3,8 @@ package com.name.cn.mydairly;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.name.cn.mydairly.data.DaoSession;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
